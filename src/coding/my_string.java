360f60fc221c8dc940814ad43ed7358763fd5882
leemdongjoon2267 <dlaehdwns2020@naver.com>
2024-07-19 10:54:55 +0900

    fourth coding

---
+++ src/coding/my_string.java
@@ -0,0 +1,13 @@
+package coding;
+
+public class my_string {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        for(int i = 0; i<k; i++){
+            answer+=my_string;
+        }
+        return answer;
+    }
+}
+
+
